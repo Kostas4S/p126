3dd85876df6b71f2c4f5c10ef0109ea79fbdb8cf
Kostas4S <81780796+Kostas4S@users.noreply.github.com>
2022-06-20 15:30:09 -0400

    Update camera.js

--- screens/camera.js
+++ screens/camera.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import {Button, Image, View, Platform} from "react-native";
 import * as ImagePicker from "expo-image-picker"
 import * as Permissions from "expo-Permissions"
+import camera
 
 export default class PickImage extends React.Component{
     state = {
